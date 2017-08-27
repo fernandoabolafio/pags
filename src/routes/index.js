04c1d5dbf518417853970a656217be55221327a1
Fernando Abolafio <fernandohsabolafio@gmail.com>
2017-08-27 19:29:03 -0300

    Small routes refactor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import AppLayout from '../layouts/AppLayout';
 import Home from './Home';
-import MainScreen from './MainScreen';
-import BankSync from './BankSync';
+import App from './App';
 
 export const createRoutes = (store) => ({
   path: '/',
   component: AppLayout,
   indexRoute: Home,
   childRoutes: [
-    BankSync,
-    MainScreen
+    App
   ]
 });
 
