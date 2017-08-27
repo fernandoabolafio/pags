b0b7e2d51bab0b586ebf90c9001e1bbb660b165a
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-08-27 11:04:40 -0300

    Fix responsiity

--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -6,32 +6,14 @@ import Heading from 'grommet/components/Heading';
 import Box from 'grommet/components/Box';
 import Meter from 'grommet/components/Meter';
 import Legend from 'grommet/components/Legend';
+import List from 'grommet/components/List';
+import ListItem from 'grommet/components/ListItem';
+import Label from 'grommet/components/Label';
+import Value from 'grommet/components/Value';
+import Anchor from 'grommet/components/Anchor';
+import Button from 'grommet/components/Button';
 import AnnotatedMeter from 'grommet-addons/components/AnnotatedMeter';
-
-// import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
-//
-// const data = [
-//       {investimento: 'CDI', percentual: [40, 50], liquido: 400},
-//       {investimento: 'CDB', percentual: 20, liquido: 200},
-//       {investimento: 'LCI', percentual: 10, liquido: 100},
-//       {investimento: 'LCA', percentual: 30, liquido: 300},
-// ];
-// const SimpleBarChart = React.createClass({
-// 	render () {
-//   	return (
-//       <ResponsiveContainer width='100%' aspect={2}>
-//       	<BarChart data={data}>
-//          <XAxis dataKey="investimento"/>
-//          <YAxis domain={[0, 100]}/>
-//          <CartesianGrid strokeDasharray="1 1"/>
-//          <Tooltip/>
-//          <Legend />
-//          <Bar dataKey="percentual" fill="#8884d8" />
-//         </BarChart>
-//       </ResponsiveContainer>
-//     );
-//   }
-// })
+import AddIcon from 'grommet/components/icons/base/Add';
 
 export default class MainScreen extends React.Component {
   render() {
@@ -49,37 +31,88 @@ export default class MainScreen extends React.Component {
     return (
       <Section>
         <Box direction='row'>
-          <Box style={{backgroundColor: 'white', width: small ? '' : '25%'}} margin={small ? 'medium' : {left: 'medium', right: 'small'}}>
+          <Box style={{backgroundColor: 'white', width: small ? '' : '35%'}} margin={small ? 'medium' : {left: 'medium', right: 'small'}}>
             <Card
             >
               Info gamification
             </Card>
           </Box>
-          <Box style={{backgroundColor: 'white', width: small ? '' : '75%'}}  margin={small ? 'medium' : {left: 'small', right: 'medium'}}>
+          <Box style={{backgroundColor: 'white', width: small ? '' : '65%'}}  margin={small ? 'medium' : {left: 'small', right: 'medium'}}>
             <Card
-              align='center'
-              justify='center'
+              style={{width:'100%'}}
               heading={
                   <Heading tag='h2'>
                     Seus Investimentos
                   </Heading>
                 }
             >
-              <AnnotatedMeter type="circle" legend={true} units="R$"
-                size={small ? 'medium' : 'medium'}  series={cpuData} />
+              <Box align='center'>
+                <AnnotatedMeter type="circle" legend={true} units="R$"
+                  size={small ? 'medium' : 'medium'}  series={cpuData} />
+              </Box>
             </Card>
           </Box>
         </Box>
-        <Box direction='row' style={{backgroundColor: 'white'}} margin={small ? 'medium' : {top: 'medium', left: 'medium', right: 'medium'}}>
-          <Card
-            heading={
-                <Heading tag='h2'>
-                  Seus Objetivos
-                </Heading>
-              }
-          >
-            Info gamification
-          </Card>
+        <Box direction='row'>
+          <Box direction='row' style={{backgroundColor: 'white', width: small ? '' : '75%'}} margin={small ? 'medium' : {top: 'medium', left: 'medium', right: 'small'}}>
+            <Card
+              style={{width:'100%'}}
+              heading={
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                  <Heading tag='h2'>
+                    Seus Objetivos
+                  </Heading>
+                  <Button onClick={() => console.log('iha')}  icon={<AddIcon />} />
+                </div>
+
+                }
+            >
+              <List selectable>
+                <ListItem justify="between" responsive={false}>
+                  <Label truncate>Objetivo ;</Label>
+                  <Box margin={{left: 'small'}}>
+                  <Value value={'50'}
+                    units='%'
+                    align='start' />
+                  <Meter value='50'/>
+                  </Box>
+                </ListItem>
+                <ListItem justify="between" responsive={false}>
+                  <Label truncate>Objetivo 1</Label>
+                  <Box>
+                  <Value value={'50'}
+                    units='%'
+                    align='start' />
+                  <Meter value='50'/>
+                  </Box>
+                </ListItem>
+                <ListItem justify="between" responsive={false}>
+                  <Label truncate>Objetivo 1</Label>
+                  <Box>
+                  <Value value={'50'}
+                    units='%'
+                    align='start' />
+                  <Meter value='50'/>
+                  </Box>
+                </ListItem>
+                <ListItem justify="between" responsive={false}>
+                  <Label truncate>Objetivo 1</Label>
+                  <Box>
+                  <Value value={'50'}
+                    units='%'
+                    align='start' />
+                  <Meter value='50'/>
+                  </Box>
+                </ListItem>
+              </List>
+            </Card>
+          </Box>
+          <Box direction='row' style={{backgroundColor: 'white', width: small ? '' : '25%'}} margin={small ? 'medium' : {top: 'medium', left: 'small', right: 'medium'}}>
+            <Card
+            >
+                Informaçao
+            </Card>
+          </Box>
         </Box>
       </Section>
     );
