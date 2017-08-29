64f46e4f660f329842d2e7c1a48532a1e1748eec
Fernando Abolafio <fernandohsabolafio@gmail.com>
2017-08-29 08:41:07 -0300

    Add helper text for tipos de investimentos

---
+++ src/components/Fundos/Entenda.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Layer from 'grommet/components/Layer';
+import Section from 'grommet/components/Section';
+import Close from 'grommet/components/icons/base/Close';
+import Box from 'grommet/components/Box';
+import Heading from 'grommet/components/Heading';
+import Paragraph from 'grommet/components/Paragraph';
+import Anchor from 'grommet/components/Anchor';
+
+export default class Entenda extends React.Component {
+
+  render() {
+    return (
+      <Layer>
+        <Section>
+          <Box align="end">
+            <Anchor  onClick={this.props.onClose} icon={<Close />} />,
+          </Box>
+          <Box align="center">
+            <Heading tag="h3">{this.props.title}</Heading>
+            <Paragraph>
+              {this.props.text}
+            </Paragraph>
+          </Box>
+        </Section>
+      </Layer>
+    )
+  }
+}
