956a20c238ab43877a2e88f391eeb9124eece502
Fernando Abolafio <fernandohsabolafio@gmail.com>
2017-08-29 12:57:13 -0300

    Fix footer colors and panel general colors (added deps RUN NPM INSTALL)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,19 +9,20 @@ import SocialShare from 'grommet/components/SocialShare';
 import Form from 'grommet/components/Form';
 import Anchor from 'grommet/components/Anchor';
 import GrommetIcon from 'grommet/components/icons/base/BrandGrommetOutline';
+import Image from 'grommet/components/Image';
 
 export const MyFooter = ({small}) =>
   <Footer >
     <Section
       align="center"
       pad="medium"
-      style={{backgroundColor: '#102027'}}
       full="horizontal"
       direction="row"
     >
       <Box basis="1/2" pad={{horizontal: 'large', vertical: 'large'}} wrap direction='row' justify='center'>
-        <Box basis='1/2' align='center' justify='center' margin={{vertical: 'small'}}>
-            <GrommetIcon colorIndex='light-1' size='large' />
+        <Box basis='1/2' align='center'  margin={{vertical: 'small'}}>
+          <Label margin="none" style={{marginBottom: "10px"}} >Parceiros</Label>
+          <Image size="small" src={'https://seeklogo.com/images/I/Itau-logo-082730CE51-seeklogo.com.png'}/>
         </Box>
         <Box basis='1/2' direction='row' justify='center' margin={{vertical: 'small'}} responsive={false}>
           <div style={{display: 'flex', flexDirection: 'column', marginRight: small ? '25px' : '10px'}}>
@@ -32,7 +33,7 @@ export const MyFooter = ({small}) =>
           </div>
           <div style={{display: 'flex', flexDirection: 'column'}}>
             <Label size={small ? 'medium' : 'small'} uppercase>Produto</Label>
-            <Anchor label={<Label size={small ? 'medium' : 'small'}>Por que ToRyca?</Label>}></Anchor>
+            <Anchor label={<Label size={small ? 'medium' : 'small'}>Por que Pag$?</Label>}></Anchor>
             <Anchor label={<Label size={small ? 'medium' : 'small'}>Depoimentos</Label>}></Anchor>
             <Anchor label={<Label size={small ? 'medium' : 'small'}>Segurança</Label>}></Anchor>
             <Anchor label={<Label size={small ? 'medium' : 'small'}>Educacional</Label>}></Anchor>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './index.scss';
 import MyApp from './App';
 import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/grommet-css'
-import '../node_modules/grommet/scss/vanilla/index.scss';
+// import '../node_modules/grommet-css'
+
+
 
 ReactDOM.render(<MyApp />, document.getElementById('root'));
 registerServiceWorker();
