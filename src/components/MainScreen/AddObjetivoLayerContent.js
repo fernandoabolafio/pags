7d31435b2352d83ea4b298ae11e9a65947305210
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-08-29 04:48:47 -0300

    MainScreen enhancement

--- src/components/MainScreen/AddObjetivoLayerContent.js
+++ src/components/MainScreen/AddObjetivoLayerContent.js
@@ -9,39 +9,67 @@ import Heading from 'grommet/components/Heading';
 import Box from 'grommet/components/Box';
 import Button from 'grommet/components/Button';
 
-export default class ViewObjetivoLayerContent extends React.Component {
+export default class AddObjetivoLayerContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      valor: '',
+      previsao_de_termino: '',
+      descricao: ''
+    }
+  }
+
+  handleInputChange = (event) => {
+    const {value, name} = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit = () => {
+    console.log(this.state);
+    this.props.addObjetivo(this.state);
+    this.props.onClose();
+  }
+
   render() {
+    const {nome, valor, previsao_de_termino, descricao} = this.state;
     const {onClose, small} = this.props;
     return (
       <Section style={ small ? {} : {minHeight: '480px', minWidth: '548px'}}>
       <Heading tag="h3" margin="none">Adicionar Objetivo</Heading>
       <Box direction='column'>
-        <Form pad={{vertical: 'medium'}} >
+        <Form pad={{vertical: 'medium'}}>
           <Box pad={{between: 'medium'}}>
             <FormField
               label='Nome'
             >
-              <TextInput />
+              <TextInput name='nome' value={nome} onDOMChange={this.handleInputChange} />
             </FormField>
             <FormField
               label='Valor'
             >
-              <TextInput />
+              <TextInput name='valor' value={valor} onDOMChange={this.handleInputChange} />
             </FormField>
             <FormField
               label='Previsão de término'
             >
-              <TextInput />
+              <TextInput name='previsao_de_termino' value={previsao_de_termino} onDOMChange={this.handleInputChange}/>
             </FormField>
             <FormField
               label='Descrição'
+
             >
               <textarea
+                onChange={this.handleInputChange}
+                name='descricao'
                 type='text'
+                value={descricao}
               />
             </FormField>
             <Box direction='row' justify='end' responsive={false}>
-              <Button primary onClick={onClose} label='Adicionar' />
+              <Button type='button' primary onClick={this.onSubmit} label='Adicionar' />
             </Box>
           </Box>
         </Form>
