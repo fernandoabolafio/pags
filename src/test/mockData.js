6599c983a5db44b9c57934091bd183587dae8329
Fernando Abolafio <fernandohsabolafio@gmail.com>
2017-08-29 09:49:24 -0300

    Populate user objetivos

--- src/test/mockData.js
+++ src/test/mockData.js
@@ -2,22 +2,196 @@ const itauUsers = [
   {
     id: '15000005205200584716453',
     first_name: 'Marcela',
-    thumb: 'http://www.insidedge.net/images/uploads/callout_images/Callout%20Thumbnails/our-people-linda-thumbnail.jpg'
+    thumb: 'http://www.insidedge.net/images/uploads/callout_images/Callout%20Thumbnails/our-people-linda-thumbnail.jpg',
+    objetivos: [
+      {
+        nome: 'Emergência',
+        criado: '10/10/2016',
+        conclusaoEstimada: '10/10/2017',
+        valor: 5000,
+        acumulado: 1,
+        descricao: 'Dinherio sempre disponível em caso de emergência',
+      },
+      {
+        nome: 'Cirurgia',
+        criado: '10/6/2017/',
+        valor: 30000,
+        acumulado: 0.76,
+        conclusaoEstimada: '10/6/2020',
+        descricao: 'Cirurgia de joelho da mamis'
+      },
+      {
+        nome: 'Novos Móveis',
+        criado: '10/5/2017',
+        valor: 20000,
+        acumulado: 0,
+        conclusaoEstimada: '10/10/2019',
+        descricao: 'Moveis sob encomenda para o novo AP'
+      }
+    ],
+    dica: {
+      greeting: 'Quase lá!',
+      mensagem: 'Para completar todos seus sonhos economize R$5mil por 3 anos e invista em:',
+      investimentos: [
+        {
+          name: 'Multifundos MM',
+          id: 1007
+        }
+      ]
+    }
   },
   {
     id: '15000005201200486916443',
     first_name: 'Júlia',
-    thumb: 'https://s-media-cache-ak0.pinimg.com/736x/f8/74/3b/f8743bf131ec8734f61e07e4367e40bc--freckle-photography-photography-portraits.jpg'
+    thumb: 'https://s-media-cache-ak0.pinimg.com/736x/f8/74/3b/f8743bf131ec8734f61e07e4367e40bc--freckle-photography-photography-portraits.jpg',
+    objetivos: [
+      {
+        nome: 'Planos futuros',
+        criado: '10/8/2016/',
+        conclusaoEstimada: '10/8/2017',
+        valor: 18000,
+        acumulado: 1,
+        descricao: 'Talvez viajar, talvez comprar um carro. O tempo dirá.'
+      },
+      {
+        nome: 'Bicileta',
+        criado: '10/6/2016/',
+        conclusaoEstimada: '10/6/2017',
+        valor: 2000,
+        acumulado: 1,
+        descricao: 'Bike para trilha'
+      },
+      {
+        nome: 'Computador',
+        criado: '10/6/2016/',
+        conclusaoEstimada: '10/6/2017',
+        valor: 5000,
+        acumulado: 1,
+        descricao: 'Notebook Asus 73765 viper *-*'
+      },
+      {
+        nome: 'Casa',
+        criado: '10/8/2017',
+        conclusaoEstimada: '10/8/2021',
+        valor: 100000,
+        acumulado: 0.21,
+        descricao: 'Casa com varanda em Minas Gerais'
+      }
+    ],
+    dica: {
+      greeting: 'Quase lá!',
+      mensagem: 'Para completar todos os seus sonhos economize R$17mil por 4 anos ou invista R$10mil por ano em',
+      investimentos: [
+        {
+          name: 'Excellence DI',
+          id: 1009
+        }
+      ]
+    }
   },
   {
     id: '15000005204200286316453',
     first_name: 'Ivan',
-    thumb: 'http://www.insidedge.net/images/uploads/callout_images/Callout%20Thumbnails/our-people-matt-thumbnail.jpg'
+    thumb: 'http://www.insidedge.net/images/uploads/callout_images/Callout%20Thumbnails/our-people-matt-thumbnail.jpg',
+    objetivos: [
+      {
+        nome: 'Emergência',
+        criado: '10/8/2017',
+        conclusaoEstimada: '_sempre_',
+        valor: 20000,
+        acumulado: 1,
+        descricao: 'Dinheiro sempre disponivel para emergências'
+      },
+      {
+        nome: 'Notebook',
+        criado: '10/8/2017',
+        conclusaoEstimada: '10/8/2018',
+        valor: 5000,
+        acumulado: 1,
+        descricao: 'Notebook Dell Rolanga Ross 73xWow'
+      },
+      {
+        nome: 'Anel de noivado',
+        criado: '10/8/2017',
+        conclusaoEstimada: '10/08/2019',
+        valor: 20000,
+        acumulado: 1,
+        descricao: 'Dinheiro sempre disponivel para emergências'
+      },
+      {
+        nome: 'Carro',
+        criado: '10/8/2017',
+        conclusaoEstimada: '10/08/2020',
+        valor: 60000,
+        acumulado: 0,
+        descricao: 'Carro 4x4 off Road que eu tanto quero'
+      },
+      {
+        nome: 'Casamento',
+        criado: '10/08/2017',
+        conclusaoEstimada: '10/08/2021',
+        valor: 30000,
+        acumulado: 0,
+        descricao: 'Casamento na praia (Maresias) com minha amada'
+      }
+    ],
+    dica: {
+      greeting: 'Quase lá!',
+      mensagem: 'Para completar todos seus sonhos Economize R$9mil por 4 anos e invista em:',
+      investimentos: [
+        {
+          name: 'Maxime DI',
+          id: 1011
+        },
+        {
+          name: 'Personnalité Pre Longo Prazo RF',
+          id: 1006
+        }
+      ]
+    }
   },
   {
     id: '15000005203200586716443',
     first_name: 'Ana Carolina',
-    thumb: 'https://sarasalavarria.files.wordpress.com/2010/03/img_0758-copia-2.jpg'
+    thumb: 'https://sarasalavarria.files.wordpress.com/2010/03/img_0758-copia-2.jpg',
+    objetivos: [
+      {
+        name: 'Cofrinho',
+        criado: '10/8/2017',
+        conclusaoEstimada: '_sempre_',
+        valor: 3000,
+        acumulado: 1,
+        descricao: 'Dinheiro sempre disponível'
+      },
+      {
+        name: 'Faculdade',
+        criado: '10/8/2017',
+        conclusaoEstimada: '10/08/2021',
+        valor: 40000,
+        acumulado: 1,
+        descricao: 'Faculdade de Ciências Sociais na Unip'
+      },
+      {
+        name: 'Renovar roupas',
+        criado: '10/08/2017',
+        conclusaoEstimada: '10/08/2019',
+        valor: 5000,
+        acumulado: 1,
+        descricao: 'Comprar roupas e acessorios novos'
+      },
+      {
+        name: 'Beleza',
+        criado: '10/8/2017',
+        conclusaoEstimada: '10/8/2018',
+        valor: 600,
+        acumulado: 1,
+        descricao: 'Limpeza de pele, clareamento de dente e botox no queixo'
+      }
+    ],
+    dica: {
+      greeting: 'Parabens!',
+      mensagem: 'Com essa carteira você realiza seus sonhos e ainda terá R$50mil a mais'
+    }
   }
 ];
 
