aba41c7a13484f1cce4d254a3afac4e12ad1b774
Tuanny Harumi <tuanny.harumi@luizalabs.com>
2017-08-29 00:23:08 -0300

    Use encode sans font

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import GrommetApp from 'grommet/components/Grommet';
 
+const style = {
+  'font-family': 'Encode Sans Condensed',
+}
+
 export default class AppLayout extends React.Component {
   render() {
     return (
-      <GrommetApp>
-          {this.props.children}
+      <GrommetApp style={style}>
+        {this.props.children}
       </GrommetApp>
     );
   }
